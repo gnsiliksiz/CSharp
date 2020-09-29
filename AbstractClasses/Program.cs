d2046e8cbbd3318668d5cd3654c997a33dc0a2cc
gunes <gbaygul@ozatashipyard.com>
2020-09-29 11:54:15 +0300

    1-Recap Demo ile onceki applerin bir genel tekrarı yapıldı.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //abstract classlar birer inheritance özelligidir.
+            //interface gibi abstract class new yapamıyoruz
+            //Database database = new Database();
+            //Oracle ve Sql server implemente edebiliriz.
+            Database databaseOracle = new Oracle();
+            databaseOracle.Add();
+            databaseOracle.Delete();
+
+            Database databasesql = new SqlServer();
+            databasesql.Add();
+            databasesql.Delete();
+            Console.ReadLine();
+        }
+    }
+    //interface ve virtual methodların birleşimi gibi dusunebiliriz.
+    abstract class Database
+    {
+        //Tamamlanmıs method
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        //tamamlanmamış method
+        //içi dolu olmayan virtual methodtur.
+        public abstract void Delete();
+
+    }
+    class SqlServer : Database
+    {
+        //Delete her ortamda ayrı işlem olacagı için hepsının ayrı ayrı ımpleente edılmesı gerekır.
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by  Sql Server ");
+        }
+    }
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by  Oracle");
+        }
+    }
+
+}
---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapDemo2
+{
+    //
+    class Program
+    {
+        //Loglama hepsı ıcın ayrı ıslemler gerektırdıgı ıcın ınterface kullanıldı.Cougunda aynı bır kacı farklı olasaydı vırtual yapardık.
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger(); //interface nesnesini olusturduk.
+            customerManager.Logger = new FileLogger();
+            customerManager.Logger = new SmsLogger();
+            customerManager.Add();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        //interface property tanımla
+     
+        public ILogger Logger { get; set; }
+
+        public void Add()
+        {
+            //DatabaseLogger logger = new DatabaseLogger();-Kullanımı dogru degıl.Herzaman tek bişeye bagımlı kalınıyo.
+            //ILogger logger = new DatabaseLogger();
+            Logger.Log();
+            Console.WriteLine("Customer added!");
+        }
+    }
+    //Bir classın mutlaka bir base olmalı .
+    class DatabaseLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged !");
+        }
+    }
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("log to File");
+        }
+    }
+    //Yeni eklendi.Sadece implemente edip kullandık.
+    class SmsLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to sms");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+}
