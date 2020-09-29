4c784475a0f61e460cd9050e196f933eab1cc9f7
gunes <gbaygul@ozatashipyard.com>
2020-09-29 10:52:13 +0300

    Inheritance virtual method işlemleri yapıldı

---
+++ Inheritance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[]
+            {
+                new Customer
+                { FirsName="Güneş" ,
+                  LastName="iliksiz"
+                },
+                new Student
+                { 
+                    FirsName="Cemre",
+                    LastName="İliksiz" 
+                },
+                new Person
+                { 
+                    FirsName="Pars", 
+                    LastName="İliksiz"
+                } //interface tek basına anlam ifade etmez iken inheritance anlam ifade etmektedir.
+            };
+
+            foreach(var person in persons)
+            {
+                Console.WriteLine(person.FirsName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int ID { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Person2
+    {
+
+    }
+    //Inheritance ex
+    //Bir nesne sadece bir kere inheritance alınabılır.Ama birden cok implemente edebılırsınız.
+    class Customer:Person // ,Person2 Ata tek olur 
+    {
+        public String City { get; set; }
+    }
+    class Student : Person
+    {
+
+    }
+}
---
+++ VirtualMethod/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySqlServer mySqlServer = new MySqlServer();
+            mySqlServer.Add();
+            Console.ReadLine();
+        }
+    }
+    class DataBase
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by default");
+        }
+    }
+    class SqlServer :DataBase
+    {
+        //sql server crud işlemleri farklı oldugu için sanal methodlar ile override yapılır.
+        //Sql Server Base Database 
+        //Virtual olan override edilebilmektedir.
+        public override void Add()
+        {
+            //override ile bir önceki temel kod ezilir.
+            Console.WriteLine("Added by Sql Code");
+            //base atası demek miras alınan 
+            base.Add();
+        }
+        public override void Delete()
+        {
+            base.Delete();
+        }
+    }
+    class MySqlServer:DataBase
+    {
+        public override void Add()
+        {
+            base.Add();
+        }
+        public override void Delete()
+        {
+            base.Delete();
+        }
+    }
+}
